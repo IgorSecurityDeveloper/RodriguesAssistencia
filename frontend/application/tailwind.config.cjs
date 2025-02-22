7ef67ffa0d1d0535f6a37be1f41d1461bdd4080d
Igor Amaral Silva <igoramarals96@gmail.com>
2025-02-22 12:46:16 -0300

    Header adjustment
    Header and responsivity was created.

--- frontend/application/tailwind.config.cjs
+++ frontend/application/tailwind.config.cjs
@@ -2,6 +2,13 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		color: {
+			white: '#FFFFFF',
+			primary: '#04273E',
+		  },
+		  fontFamily: {
+			assistant: ['Assistant', 'sans-serif'],
+		  }
 	},
 	plugins: [],
 };
