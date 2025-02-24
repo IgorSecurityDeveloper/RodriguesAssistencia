651f0c2bac5d779f2322e313792e13618f740259
Igor Amaral Silva <igoramarals96@gmail.com>
2025-02-23 23:37:13 -0300

    Develop
    - Add low weigh images in the header
    - Created a Loader
    - Site can now recognize the device and send the specified images to save memory

--- frontend/application/tailwind.config.cjs
+++ frontend/application/tailwind.config.cjs
@@ -2,10 +2,12 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
-		color: {
+		colors: {
 			white: '#FFFFFF',
-			primary: '#04273E',
-		  },
+			bgblue: '#04273E',
+			bgblue2: '#05304C',
+			filter: '#36454B',
+		},
 		  fontFamily: {
 			assistant: ['Assistant', 'sans-serif'],
 		  }
